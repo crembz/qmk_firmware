4974af064b48e546a8e0db802c8b963e170b9e0e
crembz <alexanderschembri@gmail.com>
2022-08-12 23:04:59 +1000

    modified:   keyboards/splitkb/kyria/keymaps/harvester/keymap.c

--- keyboards/splitkb/kyria/keymaps/harvester/keymap.c
+++ keyboards/splitkb/kyria/keymaps/harvester/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
        KC_GRV,   KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,                                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSLS,
       KC_LSFT,   KC_A,   KC_S,   KC_D,    KC_F,    KC_G,                                            KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  RSFT_T(KC_QUOT),
-     KC_LCTRL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,XXXXXXX,      XXXXXXX, KC_RBRC,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RCTRL,
+     KC_LCTRL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,A(KC_TAB),      C(A(KC_TAB)), KC_RBRC,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RCTRL,
                               KC_LGUI, KC_LALT,   MEDIA,     NAV,    MSE,          SYM,     NUM,     FUN, KC_RALT, KC_APP
     ),
 
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NAV] = LAYOUT(
      XXXXXXX,    KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT,    XXXXXXX,                    C(KC_Y), C(KC_V), C(KC_C), C(KC_X), C(KC_Z), C(KC_S),
      XXXXXXX, C(KC_A),   C(KC_S), XXXXXXX,  C(KC_F), XXXXXXX,                       KC_CAPS,  KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, XXXXXXX,
-     XXXXXXX,  C(KC_Z), C(KC_Z), C(KC_C), C(KC_V), XXXXXXX, XXXXXXX,XXXXXXX,      XXXXXXX, XXXXXXX, KC_INS, KC_HOME, KC_PGDN, KC_PGUP, KC_END, XXXXXXX,
+     XXXXXXX,  C(KC_Z), C(KC_X), C(KC_C), C(KC_V), XXXXXXX, XXXXXXX,XXXXXXX,      XXXXXXX, XXXXXXX, KC_INS, KC_HOME, KC_PGDN, KC_PGUP, KC_END, XXXXXXX,
                               XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX
     ),
 
@@ -159,8 +159,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NUM] = LAYOUT(
       XXXXXXX, KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC,                                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
       XXXXXXX, KC_SCLN, KC_4, KC_5, KC_6, KC_EQL,                                       XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_LGUI, XXXXXXX,
-      XXXXXXX, KC_GRV, KC_1, KC_2, KC_3, KC_BSLS, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                        KC_NUM, XXXXXXX, KC_DOT,   KC_0,  KC_MINS,   XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX
+      XXXXXXX, KC_GRV, KC_1, KC_2, KC_3, KC_BSLS, XXXXXXX, KC_CALC,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                        KC_NUM,  KC_0, KC_0, KC_DOT,  KC_MINS,   XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX
     ),
 
 /*
