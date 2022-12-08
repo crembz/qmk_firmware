a6948753f21f554ba3389e85611151b046239927
lalalademaxiya1 <66767061+lalalademaxiya1@users.noreply.github.com>
2022-12-09 00:43:28 +0800

    Update Q1 and Q2 for keychron (#18713)
    Co-authored-by: Drashna Jaelre <drashna@live.com>
    Co-authored-by: jpe230 <pablin.123.ra@gmail.com>

--- keyboards/keychron/q1/q1.c
+++ keyboards/keychron/q1/q1.c
@@ -16,6 +16,17 @@
 
 #include "q1.h"
 
+const matrix_row_t matrix_mask[] = {
+    0b0111111111111101,
+    0b0111111111111111,
+    0b0111111111111111,
+    0b0111111111111111,
+    0b0111111111111111,
+    0b0111111111111111,
+};
+
+#ifdef DIP_SWITCH_ENABLE
+
 bool dip_switch_update_kb(uint8_t index, bool active) {
     if (!dip_switch_update_user(index, active)) { return false;}
     if (index == 0) {
@@ -23,3 +34,5 @@ bool dip_switch_update_kb(uint8_t index, bool active) {
     }
     return true;
 }
+
+#endif // DIP_SWITCH_ENABLE
