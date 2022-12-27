4a13ea558894ecb1c5617e641d218bdae9cb6c24
crembz <alexnderschembri@gmail.com>
2022-12-27 12:30:02 +1100

    Reviung Harvester update

--- keyboards/reviung/reviung41/keymaps/harvester/keymap.c
+++ keyboards/reviung/reviung41/keymaps/harvester/keymap.c
@@ -124,32 +124,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_NAV] = LAYOUT_reviung41(
     KC_GRV,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, GAME,            C(KC_Y), C(KC_V), C(KC_C), C(KC_X), C(KC_Z), C(KC_S),
-    _______,  C(KC_A), C(KC_S), XXXXXXX, C(KC_F), XXXXXXX,            KC_CAPS, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, XXXXXXX,
+    _______,  C(KC_A), C(KC_S), XXXXXXX, C(KC_F), XXXXXXX,            KC_CAPS, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______,
     KC_LCBR,  C(KC_Z), C(KC_X), C(KC_C), C(KC_V), XXXXXXX,            KC_INS, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_RCBR,
                                             KC_LGUI,   _______,  KC_ENT,  KC_DEL,  XXXXXXX
   ),
 
   [_MEDIA] = LAYOUT_reviung41(
     XXXXXXX  , XXXXXXX , XXXXXXX , G(KC_E) , G(KC_R) , XXXXXXX,        RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX,
-    _______, XXXXXXX, LSG(KC_S), XXXXXXX,  XXXXXXX, XXXXXXX,              XXXXXXX, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, XXXXXXX,
+    _______, XXXXXXX, LSG(KC_S), XXXXXXX,  XXXXXXX, XXXXXXX,              XXXXXXX, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                             _______,   XXXXXXX,  KC_MPLY, KC_MSTP, KC_MUTE
   ),
   [_NUM] = LAYOUT_reviung41(
     XXXXXXX, KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-      XXXXXXX, KC_SCLN, KC_4, KC_5, KC_6, KC_EQL,                      KC_NUM, KC_RSFT, KC_RCTL, KC_RALT, KC_LGUI, XXXXXXX,
+      _______, KC_SCLN, KC_4, KC_5, KC_6, KC_EQL,                      KC_NUM, KC_RSFT, KC_RCTL, KC_RALT, KC_LGUI, _______,
     KC_LBRC,   KC_GRV, KC_1, KC_2, KC_3, KC_BSLS,            QK_BOOT,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_RBRC,
                                             KC_DOT,   KC_0,  KC_MINS,  _______,  XXXXXXX
   ),
   [_SYM] = LAYOUT_reviung41(
     XXXXXXX, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-      XXXXXXX, KC_COLN, KC_DLR, KC_PERC, KC_CIRC, KC_PLUS,                      XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_LGUI, XXXXXXX,
+      _______, KC_COLN, KC_DLR, KC_PERC, KC_CIRC, KC_PLUS,                      XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_LGUI, _______,
     XXXXXXX,   KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_PIPE,            XXXXXXX,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
                                             KC_LPRN, KC_RPRN, KC_UNDS,  XXXXXXX,  _______
   ),
   [_FUN] = LAYOUT_reviung41(
     XXXXXXX, KC_F12, KC_F7, KC_F8, KC_F9, KC_PSCR,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,
-      XXXXXXX, KC_F11, KC_F4, KC_F5, KC_F6, KC_SCRL,                      XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_LGUI, XXXXXXX,
+      _______, KC_F11, KC_F4, KC_F5, KC_F6, KC_SCRL,                      XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_LGUI, _______,
     XXXXXXX,   KC_F10, KC_F1, KC_F2, KC_F3, KC_PAUS,            XXXXXXX,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  EE_CLR,
                                             KC_MENU,   KC_TAB,  _______,  XXXXXXX,  XXXXXXX
   ),
